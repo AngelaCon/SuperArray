41d028d0ab7e1d46cc52c8c86626c60136e667af
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2020-11-03 17:38:59 -0500

    done with methods

--- SuperArray.java
+++ SuperArray.java
@@ -18,20 +18,22 @@ public class SuperArray {
     private void resize() {
         String[] arr = new String[2*size];
         for (int i = 0; i < data.length; i++) {
-            data[i] == arr[i];
+            data[i] = arr[i];
         }
         data = arr;
     }
 
     public boolean add(String element) {
         if (size == data.length) resize();
-        size += 1
+        size += 1;
         data[size+1] = element;
+        return true;
     }
 
-
     public String set(int index, String element) {
-
+        String replaced = data[index];
+        data[index] = element;
+        return replaced;
     }
 
 
