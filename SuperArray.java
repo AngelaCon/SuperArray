a2541f09027b3e92c08c3db2ef702e1795ba4810
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2020-11-04 13:05:08 -0500

    tested tostring and clear

--- SuperArray.java
+++ SuperArray.java
@@ -36,6 +36,34 @@ public class SuperArray {
         return replace;
     }
 
+    public boolean isEmpty(){
+        return(size==0);
+    }
+
+    public void clear(){
+        size = 0;
+    }
+
+    public String toString(){
+        String array = "[";
+        if (size == 0) array = "[]";
+        for (int i = 0; i < size; i++) {
+            array += String.valueOf(data[i]) + ", ";
+        }
+        array += ("]");
+        return array;
+    }
+
+    // public boolean contains(String s) {
+    //     s = data.toString();
+    //     for (int i = 0; i < s.length(); i++) {
+    //         if
+    //     }
+    // }
+
+
+
+
 
 
 }
