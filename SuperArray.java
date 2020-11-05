b4e7276193669fdf1240b8256386bbcbce912ce3
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2020-11-05 14:51:57 -0500

    done with methods

--- SuperArray.java
+++ SuperArray.java
@@ -7,6 +7,12 @@ public class SuperArray {
         size = 0;
     }
 
+    public SuperArray(int initialCapacity) {
+        data = new String[initialCapacity];
+        size = 0;
+    }
+
+
     public int size() {
         return size;
     }
@@ -30,6 +36,16 @@ public class SuperArray {
         return true;
     }
 
+    public void add(int index, String element) {
+        if (size == data.length) resize();
+        for(int i=size-1;i>=index;i--){
+            data[i+1] = data[i];
+        }
+        data[index] = element;
+        size++;
+    }
+
+
     public String set(int index, String element) {
         String replace = data[index];
         data[index] = element;
@@ -48,22 +64,46 @@ public class SuperArray {
         String array = "[";
         if (size == 0) array = "[]";
         for (int i = 0; i < size; i++) {
-            array += String.valueOf(data[i]) + ", ";
+            if (i == size - 1) array += String.valueOf(data[i]) + "]";
+            else array += String.valueOf(data[i]) + ", ";
         }
-        array += ("]");
         return array;
     }
 
-    // public boolean contains(String s) {
-    //     s = data.toString();
-    //     for (int i = 0; i < s.length(); i++) {
-    //         if
-    //     }
-    // }
+    public boolean contains(String s) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].equals(s)) return true;
+        }
+        return false;
+    }
 
+    public String remove(int index) {
+        for (int i = index; i < size-1; i++) {
+            data[i]= data[i+1];
+        }
+        String removed = data[index];
+        size--;
+        return removed;
+    }
 
+    public int indexOf(String s) {
+        boolean found = false;
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (found == false && data[i].equals(s)) {
+                found = true;
+                index = i;
+            }
+        }
+        return index;
+    }
 
-
-
+    public String[] toArray() {
+        String array[]= new String[size];
+        for (int i = 0; i < data.length; i++) {
+            array[i] = data[i];
+        }
+        return array;
+    }
 
 }
